8ba55acc0368320da09f729ae0dd569a0594ced4
dawitel <dawiteliaskassaye@gmail.com>
2024-08-26 13:56:33 +0300

    fix color

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -35,7 +35,6 @@ const InputForm: React.FC = () => {
         />
         <ChooseDataFormat />
         <button
-          className="inline-flex items-center justify-center text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background bg-primary text-primary-foreground hover:bg-primary/90 h-11 px-8 rounded-md"
           type="submit"
         >
           Scrape 🔥
