858da24226b9e0162437c7139f218b73fc22105e
dawitel <dawiteliaskassaye@gmail.com>
2024-10-12 11:38:42 +0300

    final fix for ranking with scrape.do

--- src/app/api/v1/scrape/route.ts
+++ src/app/api/v1/scrape/route.ts
@@ -29,14 +29,13 @@ export async function POST(req: Request) {
 
     const parsedData = Parser(filePath);
     console.log(`Parsed data: ${parsedData}`);
-
+    
     const browser = await puppeteer.launch({
       executablePath: executablePath(),
       headless: true,
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
-        `--proxy-server=${smartProxyEndpoint}`,
       ],
     }); 
     const retry = 3
@@ -44,21 +43,21 @@ export async function POST(req: Request) {
     for (const item of parsedData) {
       const ebayUrl = item["eBay URL"];
       const identity = item["Identity"];
-
+      
       if (!ebayUrl) {
         logger.warn(`Skipping item with no eBay URL: ${JSON.stringify(item)}`);
         continue;
       }
-
+      
       const data = await scraper(ebayUrl, storeName, retry,  identity, browser);
       scrapedData.push(data);
-
+      
       const randomDelay = Math.floor(Math.random() * (15000 - 5000) + 5000);
       await new Promise((resolve) => setTimeout(resolve, randomDelay));
     }
-
+    
     await browser.close();
-    console.log(`Scraped data: ${JSON.stringify(scrapedData)}`);
+    console.log(`Scraped data: ${scrapedData}`);
 
     saveData(scrapedData);
     logger.info("Scraped data has been persisted locally");
